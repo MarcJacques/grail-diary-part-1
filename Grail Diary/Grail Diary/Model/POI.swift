31abff813114b623e4c68feda4938aa52b716b02
Marc <marc.daniel.jacques@gmail.com>
2020-10-30 13:35:59 -0400

    organized files

---
+++ Grail Diary/Grail Diary/Model/POI.swift
@@ -0,0 +1,14 @@
+//
+//  POI.swift
+//  Grail Diary
+//
+//  Created by Marc Jacques on 10/30/20.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    let clues: [String]
+}
---
+++ Grail Diary/Grail Diary/Views/POITableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  POITableViewCell.swift
+//  Grail Diary
+//
+//  Created by Marc Jacques on 10/30/20.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
